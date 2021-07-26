932f76c7e0eb4060a37f6f008a045f745bf97d90
doublebyte1 <info@doublebyte.net>
2021-07-26 16:08:40 +0200

    - added build

---
+++ build/precache-manifest.ed50fcaf55b09326c9c3e8c717ec1e44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5799dafc54cb41adaaefa8243f1ca27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21656a7ecba59a58bd4b",
+    "url": "/static/css/main.9ede2c25.chunk.css"
+  },
+  {
+    "revision": "21576566917dc02c6621",
+    "url": "/static/js/2.4be0f53a.chunk.js"
+  },
+  {
+    "revision": "21656a7ecba59a58bd4b",
+    "url": "/static/js/main.8387a30a.chunk.js"
+  },
+  {
+    "revision": "65528ff0e5a41dde5d8d",
+    "url": "/static/js/runtime-main.03dd6308.js"
+  }
+]);
