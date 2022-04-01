05ce185eb4372b5bb1255de3e1ab24b924a598cb
doublebyte1 <info@doublebyte.net>
2022-04-01 11:45:57 +0200

    - refactored for new metadata deployment

--- build/precache-manifest.1c79f0bc57c8f63f33bf43306707ad14.js
+++ build/precache-manifest.1c79f0bc57c8f63f33bf43306707ad14.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f85490da53f1d008bd1f328fe41008b",
+    "revision": "ff7b69b0e270c77513c6f45b4128d7db",
     "url": "/record-viewer/index.html"
   },
   {
-    "revision": "a9ddffd7f191a172786e",
+    "revision": "562fc648189933f403ae",
     "url": "/record-viewer/static/css/main.9ede2c25.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/record-viewer/static/js/2.4be0f53a.chunk.js"
   },
   {
-    "revision": "a9ddffd7f191a172786e",
-    "url": "/record-viewer/static/js/main.d54e0b40.chunk.js"
+    "revision": "562fc648189933f403ae",
+    "url": "/record-viewer/static/js/main.1e69bcfa.chunk.js"
   },
   {
     "revision": "354b9698597156c0d78d",
