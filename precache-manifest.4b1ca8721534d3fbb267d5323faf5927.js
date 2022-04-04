14a13d7d925ebaa93db8601ad946012ac2de1aea
doublebyte1 <info@doublebyte.net>
2022-04-04 12:42:07 +0200

    Updates

---
+++ precache-manifest.4b1ca8721534d3fbb267d5323faf5927.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72786a41b94ac3d1a1cb60c7f3e2c2ea",
+    "url": "/record-viewer/index.html"
+  },
+  {
+    "revision": "d855c61c53f7201bcf71",
+    "url": "/record-viewer/static/css/main.8366ca33.chunk.css"
+  },
+  {
+    "revision": "0bf239dbe7824aa67841",
+    "url": "/record-viewer/static/js/2.0b91a507.chunk.js"
+  },
+  {
+    "revision": "d855c61c53f7201bcf71",
+    "url": "/record-viewer/static/js/main.f4949c16.chunk.js"
+  },
+  {
+    "revision": "354b9698597156c0d78d",
+    "url": "/record-viewer/static/js/runtime-main.0f96c7bc.js"
+  }
+]);
